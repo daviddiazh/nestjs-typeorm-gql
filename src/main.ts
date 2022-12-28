218fb8a5b41d069c3a79bd4bcce89dc7d1be2fce
daviddiazh <daviddiazlds1221@gmail.com>
2022-12-28 00:55:28 -0500

    Adding setup for prefix /api in entire app

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function mainApplication() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(8080);
 
-  console.log('🚀 Server running on port', 8080)
+  console.log('🚀 Server running on port', 8080);
 }
 
 mainApplication();
