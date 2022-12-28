8a3378560ff590b5b0b43e541fa0384f0f5224ee
daviddiazh <daviddiazlds1221@gmail.com>
2022-12-28 08:51:25 -0500

    Adding validations with class-validator

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import { IsArray, IsIn, IsNotEmpty, IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
+
+export class CreateProductDto {
+
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsOptional()
+    @IsNumber()
+    @IsPositive()
+    price?: number;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsString()
+    slug?: string;
+
+    @IsOptional()
+    @IsNumber()
+    stock?: number;
+
+    @IsNotEmpty()
+    @IsArray()
+    @IsString({ each: true })
+    sizes: string[];
+
+    @IsNotEmpty()
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    gender: string;
+
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -4,23 +4,23 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
+  async create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all products`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} product`;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  async update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} product`;
   }
 }
