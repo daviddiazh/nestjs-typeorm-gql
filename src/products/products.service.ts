901f85cbfed2d371c8183759357fc4f5d7b8ec74
daviddiazh <daviddiazlds1221@gmail.com>
2022-12-28 09:21:03 -0500

    Making create and save data in DB

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+
+  private readonly logger = new Logger('ProductsService');
+
+  constructor(
+    @InjectRepository( Product )
+    private readonly productRepository: Repository<Product>
+  ){}
+
   async create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    try {
+      const product = this.productRepository.create( createProductDto );
+      await this.productRepository.save( product );
+
+      return product;
+    } catch (error) {
+      this.handleDBExceptions( error );
+    }
   }
 
   async findAll() {
@@ -23,4 +41,16 @@ export class ProductsService {
   async remove(id: number) {
     return `This action removes a #${id} product`;
   }
+
+
+  private handleDBExceptions (error: any) {
+
+    if( error.code === '23505' ) {
+      throw new BadRequestException(error.detail)
+    }
+
+    this.logger.debug(error);
+    throw new InternalServerErrorException('Unexpected error, make sure of check server logs')
+  }
+
 }
