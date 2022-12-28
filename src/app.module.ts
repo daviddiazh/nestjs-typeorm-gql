eed82372e1980061a8358e821820324ff7cc8a88
daviddiazh <daviddiazlds1221@gmail.com>
2022-12-28 11:53:22 -0500

    Implementing GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
 import { ProductsModule } from './products/products.module';
+import { GraphqlEntryModule } from './graphql-entry/graphql-entry.module';
 
 @Module({
   imports: [
@@ -19,6 +24,18 @@ import { ProductsModule } from './products/products.module';
     }),
 
     ProductsModule,
+
+    GraphqlEntryModule,
+
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault
+      ]
+    }),
   ],
   controllers: [],
   providers: [],
